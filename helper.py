3663a744a98f93227f8359d86ba47b2afbd22cf3
UNKNOWN <84973544+6456yrgfbfhg@users.noreply.github.com>
2021-08-02 13:50:37 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,4 +3,4 @@ start_text = '''This bot is for Reading Nhentai Mangas
 Use /help to know commands and how to use this bot'''
 
 help_text = '''List of commands:
-/nh <code of Doujin> : to search a Nhentai Manga by search
+/nh <code of Doujin> : to search a Nhentai Manga by search'''
